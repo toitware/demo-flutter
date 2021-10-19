d067f962767c93bfe34cd11bb36ffa8906c0b00e
Florian Loitsch <florian@toitware.com>
2021-10-19 15:50:43 +0200

    Add a device selector to the 'run' example.

--- demo/lib/src/device_run.dart
+++ demo/lib/src/device_run.dart
@@ -4,15 +4,16 @@
 
 import 'dart:convert';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'device_select.dart';
 import 'toit_api.dart';
 import 'package:flutter/material.dart';
 import 'package:toit_api/toit/api/program.pbgrpc.dart' as toit;
 import 'package:uuid/uuid.dart';
 
-final deviceId = "b01fb57f-ef2a-4632-801c-c5d0f5ba5736";
-
 /// Runs a small program on a device.
-class RunPage extends StatefulWidget {
+class RunPage extends ConsumerStatefulWidget {
   final ToitApi _toitApi;
 
   RunPage(this._toitApi, {Key? key}) : super(key: key);
@@ -21,15 +22,18 @@ class RunPage extends StatefulWidget {
   _RunState createState() => _RunState();
 }
 
-class _RunState extends State<RunPage> {
-  String _text = "";
+class _RunState extends ConsumerState<RunPage> {
+  String? _text = "";
 
-  Future<void> _sendProgram() async {
+  Future<void> _sendProgram(String selectedDevice) async {
     var sources = toit.ProgramSource_Files(entryFilename: 'main.toit', files: {
       'main.toit': utf8.encode('main: print "hello world"'),
     });
+    setState(() {
+      _text = null;
+    });
     var request = toit.DeviceRunRequest(
-      deviceId: Uuid.parse(deviceId),
+      deviceId: Uuid.parse(selectedDevice),
       source: toit.ProgramSource(files: sources),
     );
     var response = widget._toitApi.programServiceStub.deviceRun(request);
@@ -37,20 +41,28 @@ class _RunState extends State<RunPage> {
       var line = utf8
           .decode(responseLine.hasErr() ? responseLine.err : responseLine.out);
       setState(() {
-        _text += line;
+        _text = (_text ?? "") + line;
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    var selectedDevice = ref.watch(selectedDeviceProvider("run")).state;
     return Scaffold(
         appBar: AppBar(
           title: Text('Toit Demo Home Page'),
         ),
         body: Column(children: [
-          TextButton(onPressed: _sendProgram, child: Text('Run')),
-          Text(_text),
+          DeviceSelector("run", widget._toitApi),
+          TextButton(
+              onPressed: selectedDevice == null
+                  ? null
+                  : () {
+                      _sendProgram(selectedDevice);
+                    },
+              child: Text('Run')),
+          _text == null ? CircularProgressIndicator() : Text(_text!),
         ]));
   }
 }
